c3d76db7ba1e1cbb24e45378da83d10fa43468b7
Myles Schwartz <mylesschwartz@Myless-MacBook-Air.local>
2022-09-05 13:09:28 -0400

    [upadte]

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import { image, images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt='g letter'/>
+    </div>
+
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt='about_spoon' className='spoon__img' />
+        <p className='p__opensans'>The Dripolator Coffeehouse has been proudly supporting your coffee habit since 1999. We buy some of the finest green coffee beans available and roast them off-site on an American-made Diedrich roaster.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt='about_kife'/>
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt='about_spoon' className='spoon__img' />
+        <p className='p__opensans'>The Dripolator Coffeehouse has been proudly supporting your coffee habit since 1999. We buy some of the finest green coffee beans available and roast them off-site on an American-made Diedrich roaster.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Where coffee is art"/>
+      <h1 className='app__header-h1'>Expresso Yourself</h1>
+      <p className='p__opensans' style={{ margin: '2rem 0'}}>As roasters, we deeply respect farmers’ dedication to excellence and sustainability, and we strive to honor their hard work by carefully developing each harvest’s optimal flavors. As baristas, we are invested in brewing coffee that showcases nature’s genius and in sharing it meticulously and enthusiastically with you.</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+    
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header img'/>
+    </div>
   </div>
 );
 
